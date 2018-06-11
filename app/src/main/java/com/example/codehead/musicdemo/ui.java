6bb3fa9acba39b418b3948f9c25b75197bc51f1c
harshcto <harshcto@gmail.com>
2018-06-11 15:39:11 +0530

    First commit

---
+++ app/src/main/java/com/example/codehead/musicdemo/ui.java
@@ -0,0 +1,34 @@
+package com.example.codehead.musicdemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class ui extends AppCompatActivity{
+
+Button b1;
+    @Override
+    public void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ui);
+        b1=findViewById(R.id.b12);
+        b1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(ui.this,MainActivity.class);
+                startActivity(intent);
+            }
+
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+
+
+}
